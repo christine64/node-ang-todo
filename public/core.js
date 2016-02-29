c130d8685e052f7f5559ca2fea7d282e438073d2
Christine Ho <christine_ho@hotmail.co.uk>
2016-02-29 17:42:31 +1100

    adding angular components to html page and angular app functionality

--- public/core.js
+++ public/core.js
@@ -1,17 +1,60 @@
-angular.module("ang-app", [])
-  .controller("mainController", mainController);
+var toDo = angular.module("ang-app", []);
 
-function mainController($rootScope, $scope) {
-  var self = this;
+function mainController($scope, $http) {
+  $scope.formData = {};
+}
 
-  this.formData = {};
-
+  //show todos
   $http.get("/api/todos")
-    .done(function(data){
-      this.todos = data;
+    .success(function(data) {
+      $scope.todos = data;
       console.log(data);
     })
     .error(function(data) {
       console.log("Error " + data);
-    })
+    });
+
+
+    //when submitting 
+    $scope.createTodo = function() {
+      $http.post("/api/todos", $scope.formData)
+        .success(function(data) {
+          $scope.formData {};
+          $scope.todos = data;
+          console.log(data);
+        })
+        .error(function(data) {
+          console.log("Error " + data);
+        });
+    };
+
+    //delete
+    $scope.deleteTodo = function(id) {
+      $http.delete("/api/todos" + id) 
+        .success(function(data) {
+          $scope.todos = data;
+          console.log(data);
+        })
+        .error(function(data) {
+          console.log("Error " + data);
+        });
+
+    };
+
 }
+//   .controller("mainController", mainController);
+
+// function mainController($rootScope, $scope) {
+//   var self = this;
+
+//   this.formData = {};
+
+//   $http.get("/api/todos")
+//     .done(function(data){
+//       this.todos = data;
+//       console.log(data);
+//     })
+//     .error(function(data) {
+//       console.log("Error " + data);
+//     })
+// }
